28fd9e91402d54060963fb1a4af581a42b8667aa
Mazzi Group <158308731+Mazzideno@users.noreply.github.com>
2026-06-13 12:53:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,32 @@
-const CACHE_NAME = 'mazzi-editor-v1';
+const CACHE_NAME = 'mazzi-editor-v2';
 const FILES_TO_CACHE = [
   './',
+  './MazziGroup-PostEditor-OFFLINE.html',
   './index.html',
   './logo.png',
   './bg1.jpg',
-  './bg2.jpg'
+  './bg2.JPG',
+  './manifest.json'
 ];
 
-// Install - cache all files
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate - clean old caches
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
   );
   self.clients.claim();
 });
 
-// Fetch - serve from cache, fallback to network
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request).then((fetchResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(e.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      }).catch(() => {
-        return caches.match('./index.html');
-      });
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
